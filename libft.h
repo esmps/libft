b2e510de517f1a8130fd7d9a64d8721483b9b8dd
Emma Pines-Schwartz <emma.pines.s@gmail.com>
2020-03-20 13:37:15 -0700

    fixed misspelling

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: epines-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 16:13:11 by epines-s          #+#    #+#             */
-/*   Updated: 2020/03/10 22:21:16 by epines-s         ###   ########.fr       */
+/*   Updated: 2020/03/20 13:36:59 by epines-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ size_t				ft_strlen(const char *s);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 void				ft_lstclear(t_list **lst, void (*del)(void *));
-void				t_lstdelone(t_list *lst, void (*del)(void *));
+void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstlast(t_list *lst);
 t_list				*ft_lstmap(t_list *lst, \
